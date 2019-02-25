3abddc4f676198bc2cb4f1daf08e5a0f0acb7ac4
HASH <41845819+hashplatform@users.noreply.github.com>
2019-02-25 09:44:45 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 71316;
+static const int PROTOCOL_VERSION = 71317;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 210;
@@ -21,7 +21,7 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71315;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71316;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71317;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
